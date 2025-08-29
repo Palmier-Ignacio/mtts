0a05782cd1f09fb93440cb1e8958d4189fd5c195
Palmier-Ignacio <palmier.ignacio.nicolas@gmail.com>
2025-08-28 23:31:10 -0300

    cambios pedidos - email, telefono, ajustes css

--- app.js
+++ app.js
@@ -4,12 +4,49 @@ const indexRouter = require('./routes/index');
 const categoriasRouter = require("./routes/categorias")
 const productosRouter = require("./routes/productos")
 const expressLayouts = require('express-ejs-layouts');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(expressLayouts);            
 app.set('layout', 'layouts/layout'); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const transporter = nodemailer.createTransport({
+  host: "in-v3.mailjet.com",
+  port: 587,
+  auth: {
+    user: "52346dae1b601804c7b556eef83d8371",     // lo que copiaste de Mailjet
+    pass: "174233c74bbb5a35225da422e3c8efb4"   // lo que copiaste de Mailjet
+  }
+});
+
+app.post("/enviar", (req, res) => {
+  const { nombre, emailPhone, mensaje, apellido} = req.body;
+
+  const mailOptions = {
+    from: '"Palmier Ignacio" <palmier.ignacio.nicolas@gmail.com>', 
+    to: "npnaacho@gmail.com", 
+    subject: `Nuevo mensaje de ${nombre + " " + apellido} desde el formulario web`,
+    html: `
+  <h3>Nuevo mensaje desde la web.</h3>
+  <p><b>Nombre:</b> ${nombre + " " + apellido}</p>
+  <p><b>Email:</b> ${emailPhone}</p>
+  <p><b>Mensaje:</b> ${mensaje}</p>
+`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error al enviar correo:", error);
+      res.redirect("/?status=error#contacto");
+    }
+    console.log("Correo enviado:", info.response);
+    res.redirect("/?status=ok#contacto");
+  });
+});
 
 const categories = [
   { id: 'madera', title: 'Tornillos y fijaciones para madera', description: 'Gran variedad de productos con stock permanente.', image: 'tornillos_madera.jpg' },
